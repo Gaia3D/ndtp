3fbb3e80d73fd876a283c3e401a90ce6c8a1f3dc
shpark <shpark@gaia3d.com>
2020-01-08 11:30:44 +0900

    geotools 스타일 관련 디펜던시 추가, sld 파일 스타일링 임시

---
+++ ndtp-admin/src/main/java/ndtp/geospatial/LayerStyleParser.java
@@ -0,0 +1,131 @@
+package ndtp.geospatial;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import org.geotools.factory.CommonFactoryFinder;
+import org.geotools.filter.FilterFactoryImpl;
+import org.geotools.sld.SLDConfiguration;
+import org.geotools.styling.Fill;
+import org.geotools.styling.LineSymbolizer;
+import org.geotools.styling.Mark;
+import org.geotools.styling.NamedLayer;
+import org.geotools.styling.PointSymbolizer;
+import org.geotools.styling.PolygonSymbolizer;
+import org.geotools.styling.SLDTransformer;
+import org.geotools.styling.Stroke;
+import org.geotools.styling.StyleFactory;
+import org.geotools.styling.StyledLayerDescriptor;
+import org.geotools.xml.Parser;
+import org.opengis.filter.FilterFactory;
+
+import ndtp.domain.GeometryType;
+
+/**
+ * sld 파일 스타일링 
+ * @author PSH
+ *
+ */
+public class LayerStyleParser {
+
+    int count = 0;
+
+    private String geometryType;
+    private String fillValue;
+    private String fillOpacityValue;
+    private String strokeValue;
+    private String strokeWidthValue;
+    private String styleData;
+
+    public LayerStyleParser(String geometryType, String fillValue, String fillOpacityValue, String strokeValue, String strokeWidthValue, String styleData) {
+        this.geometryType = geometryType;
+        this.fillValue = fillValue;
+        this.fillOpacityValue = fillOpacityValue;
+        this.strokeValue = strokeValue;
+        this.strokeWidthValue = strokeWidthValue;
+        this.styleData = styleData;
+
+        if("".equals(this.fillOpacityValue) || this.fillOpacityValue == null) {
+            this.fillOpacityValue="1.0";
+        }
+        if("".equals(this.strokeWidthValue) || this.strokeWidthValue == null) {
+            this.strokeWidthValue="1.0";
+        }
+    }
+
+    public String getFillValue() {
+        return fillValue;
+    }
+
+    public String getFillOpacityValue() {
+        return fillOpacityValue;
+    }
+
+    public String getStrokeValue() {
+        return strokeValue;
+    }
+
+    public String getStrokeWidthValue() {
+        return strokeWidthValue;
+    }
+
+    public String getStyleData() {
+        return styleData;
+    }
+
+	public void updateLayerStyle() throws Exception {
+        StyleFactory sf = CommonFactoryFinder.getStyleFactory();
+        FilterFactory filterFactory = new FilterFactoryImpl();
+        SLDTransformer styleTransform = new SLDTransformer();
+        StyledLayerDescriptor sld = (StyledLayerDescriptor) parse();
+        NamedLayer layer = (NamedLayer) sld.getStyledLayers()[0];
+        Fill fill = sf.createFill(filterFactory.literal(this.fillValue),filterFactory.literal(this.fillOpacityValue));
+        Stroke stroke = sf.createStroke(filterFactory.literal(this.strokeValue), filterFactory.literal(this.strokeWidthValue));
+
+        if (GeometryType.POINT == GeometryType.valueOf(this.geometryType.toUpperCase())) {
+            PointSymbolizer ps = (PointSymbolizer) layer.getStyles()[0]
+                                        .featureTypeStyles()
+                                        .get(0)
+                                        .rules()
+                                        .get(0)
+                                        .symbolizers().get(0);
+//            ps.getGraphic().getMarks()[0].setFill(fill);
+//            ps.getGraphic().getMarks()[0].setStroke(stroke);
+            Mark mark = sf.getDefaultMark();
+            mark.setFill(fill);
+            mark.setStroke(stroke);
+            ps.getGraphic().graphicalSymbols().clear();
+            ps.getGraphic().graphicalSymbols().add(mark);
+            
+
+        } else if (GeometryType.LINE == GeometryType.valueOf(geometryType.toUpperCase())) {
+            LineSymbolizer ps = (LineSymbolizer) layer.getStyles()[0]
+                                    .featureTypeStyles()
+                                    .get(0)
+                                    .rules()
+                                    .get(0)
+                                    .symbolizers()
+                                    .get(0);
+            ps.setStroke(stroke);
+
+        } else if (GeometryType.POLYGON == GeometryType.valueOf(geometryType.toUpperCase())) {
+            PolygonSymbolizer ps = (PolygonSymbolizer) layer.getStyles()[0]
+                                        .featureTypeStyles()
+                                        .get(0)
+                                        .rules()
+                                        .get(0)
+                                        .symbolizers()
+                                        .get(0);
+            ps.setFill(fill);
+            ps.setStroke(stroke);
+        }
+
+        this.styleData = styleTransform.transform(sld);
+    }
+
+    private Object parse() throws Exception {
+        SLDConfiguration sld = new SLDConfiguration();
+        InputStream input = new ByteArrayInputStream(this.styleData.getBytes());
+        return new Parser(sld).parse(input);
+    }
+}
