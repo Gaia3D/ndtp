d40dcc45ef8e380af0a397b4431b376229c2b8a9
shpark <shpark@gaia3d.com>
2020-01-13 19:43:23 +0900

    관리자 환경설정 임시 커밋

--- ndtp-admin/src/main/java/ndtp/controller/PolicyController.java
+++ ndtp-admin/src/main/java/ndtp/controller/PolicyController.java
@@ -8,14 +8,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.slf4j.Slf4j;
+import ndtp.domain.Policy;
+import ndtp.service.PolicyService;
 
-@Slf4j
 @Controller
 @RequestMapping("/policy")
 public class PolicyController {
 	
+	private final PolicyService policyService;
+	
+	public PolicyController(PolicyService policyService) {
+		this.policyService = policyService;
+	}
+	
 	@GetMapping(value = "/modify")
 	public String modify(HttpServletRequest reuqet, Model model) {
+		Policy policy = policyService.getPolicy();
+		
+		model.addAttribute("policy", policy);
 		
 		return "/policy/modify";
 	}
