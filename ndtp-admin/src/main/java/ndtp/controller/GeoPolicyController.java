dffab26fb2ed1aa93608650d05c8229be2c76074
kimhjj <kimhj@gaia3d.com>
2020-01-14 14:44:15 +0900

    관리자 - 공간정보 운영정책 추가

--- ndtp-admin/src/main/java/ndtp/controller/GeoPolicyController.java
+++ ndtp-admin/src/main/java/ndtp/controller/GeoPolicyController.java
@@ -8,15 +8,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.slf4j.Slf4j;
+import ndtp.domain.GeoPolicy;
+import ndtp.service.GeoPolicyService;
 
 @Slf4j
 @Controller
 @RequestMapping("/geopolicy")
 public class GeoPolicyController {
-	
+
+	private final GeoPolicyService geoPolicyService;
+
+	public GeoPolicyController(GeoPolicyService geoPolicyService) {
+		this.geoPolicyService = geoPolicyService;
+	}
+
 	@GetMapping(value = "/modify")
 	public String modify(HttpServletRequest reuqet, Model model) {
-		
+		GeoPolicy geoPolicy = geoPolicyService.getGeoPolicy();
+
+		model.addAttribute("geoPolicy", geoPolicy);
+
 		return "/geopolicy/modify";
 	}
+
 }
