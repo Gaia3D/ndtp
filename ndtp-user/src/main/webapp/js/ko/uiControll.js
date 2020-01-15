e1e2c7df3c75c04d0247e964dbac99a5cc287add
jdcheon <jdcheon@gaia3d.com>
2020-01-15 17:14:30 +0900

    메뉴 수정

--- ndtp-user/src/main/webapp/js/ko/uiControll.js
+++ ndtp-user/src/main/webapp/js/ko/uiControll.js
@@ -1,4 +1,29 @@
 $(function() {
+	var currentUrl = location.href;
+	var defaultMenuId = "";
+	
+	$("ul.nav li[data-nav]:not(:empty)").not($(this)).each(function() {
+        $(this).removeClass("on");
+        $("#" + $(this).attr("data-nav")).hide();
+    });
+	
+	if(currentUrl === undefined || currentUrl === null || currentUrl === "") {
+		// default 활성화
+		$("#dataMenu").addClass('on');
+		$('#dataContent').toggle(true);
+		$('#contentsWrap').toggle(true);
+	} else {
+		// 다른거 활성화
+		if( currentUrl.indexOf("/data/list") >= 0) {
+			$("#dataMenu").addClass('on');
+			$('#dataContent').toggle(true);
+			$('#contentsWrap').toggle(true);
+		} else {
+			$("#converterMenu").addClass('on');
+			//$('#contentsWrap').toggle(true);
+		}
+	}
+	
 	// 상세 메뉴 닫기
 	$('button#closeLeftBtn').click(function() {
 		//$('ul.nav li[data-nav]').removeClass('on');
@@ -8,16 +33,17 @@ $(function() {
 		//$('#closeLeftBtn').toggle();
 	});
 
-/***** NAV WRAP: 메뉴 *****/
+	/***** NAV WRAP: 메뉴 *****/
     // 상세 메뉴 클릭 시 기본 동작
     $("ul.nav li[data-nav]:not(:empty)").click(function() {
         var active = $(this).attr('data-nav');
         var display = $(this).toggleClass('on').hasClass('on');
+        
         // 변환(upload-data)이 아닌 컨텐츠 클릭시 다시 지도 페이지로 돌아감 
         if(location.href.indexOf("upload") > 0 || location.href.indexOf("converter") > 0 || location.href.indexOf("group") > 0) {
         	$(this).removeClass('on');
         	var classId = $(this).attr('class');
-        	window.location="../data/list?activeContent="+classId;
+        	window.location="../data/list";
         }
         
         // 변환 클릭 이벤트시 url 변경 
@@ -73,3 +99,13 @@ $(function() {
 		$('div.districtWrap').css('display', 'none');
 	});
 });
+
+function allMenuDisplay() {
+	$("ul.nav li[data-nav]:not(:empty)").not($(this)).each(function() {
+        $(this).removeClass('on');
+        $('#' + $(this).attr('data-nav')).hide();
+    });
+
+    $('#'+ active).toggle(display);
+    $('#contentsWrap').toggle(display);
+}
