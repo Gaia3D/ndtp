36cd4033856255d56688f27313abe9cf1a472535
shpark <shpark@gaia3d.com>
2020-02-03 12:10:11 +0900

    시민 참여 서비스 추가

---
+++ ndtp-user/src/main/java/ndtp/service/impl/CivilVoiceServiceImpl.java
@@ -0,0 +1,46 @@
+package ndtp.service.impl;
+
+import java.util.List;
+
+import ndtp.domain.CivilVoice;
+import ndtp.service.CivilVoiceService;
+
+public class CivilVoiceServiceImpl implements CivilVoiceService {
+
+	@Override
+	public List<CivilVoice> getListCivilVoice(CivilVoice civilvoice) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Long getCivilVoiceTotalCount(CivilVoice civilvoice) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CivilVoice getCivilVocieById() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int insertCivilVoice(CivilVoice civilvoice) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updateCivilVoice(CivilVoice civilvoice) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteCivilVoice(CivilVoice civilvoice) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
