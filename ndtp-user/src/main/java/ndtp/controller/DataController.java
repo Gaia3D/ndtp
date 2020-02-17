3ef564df5708ca78b045ccd0c3d8d491a0bf9882
jdcheon <jdcheon@gaia3d.com>
2020-02-17 11:35:36 +0900

    사용자 role 적용

--- ndtp-user/src/main/java/ndtp/controller/DataController.java
+++ ndtp-user/src/main/java/ndtp/controller/DataController.java
@@ -16,12 +16,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.extern.slf4j.Slf4j;
+import ndtp.domain.CacheManager;
 import ndtp.domain.DataGroup;
 import ndtp.domain.DataInfo;
 import ndtp.domain.GeoPolicy;
 import ndtp.domain.Key;
 import ndtp.domain.PageType;
 import ndtp.domain.Pagination;
+import ndtp.domain.RoleKey;
 import ndtp.domain.SharingType;
 import ndtp.domain.UserPolicy;
 import ndtp.domain.UserSession;
@@ -29,6 +31,7 @@ import ndtp.service.DataGroupService;
 import ndtp.service.DataService;
 import ndtp.service.GeoPolicyService;
 import ndtp.service.UserPolicyService;
+import ndtp.support.RoleSupport;
 import ndtp.utils.DateUtils;
 
 @Slf4j
@@ -111,6 +114,9 @@ public class DataController {
 		log.info("@@ DataController list dataInfo = {}, pageNo = {}", dataInfo, pageNo);
 
 		UserSession userSession = (UserSession)request.getSession().getAttribute(Key.USER_SESSION.name());
+		
+		String roleCheckResult = roleValidator(request, userSession.getUserGroupId(), RoleKey.USER_DATA_READ.name());
+		if(roleCheckResult != null) return roleCheckResult;
 
 		GeoPolicy geoPolicy = geoPolicyService.getGeoPolicy();
 		UserPolicy userPolicy = userPolicyService.getUserPolicy(userSession.getUserId());
@@ -230,176 +236,6 @@ public class DataController {
 		return "/data/modify";
 	}
 
-//
-//	/**
-//	 * Data 정보(ajax용) - 중복이라 맘이 안 편함... 뭔가 좋은 방법이 없을까?
-//	 * @param request
-//	 * @param dataInfo
-//	 * @return
-//	 */
-//	@GetMapping(value = "detail-data-info")
-//	@ResponseBody
-//	public Map<String, Object> detailDataInfo(HttpServletRequest request, DataInfo dataInfo) {
-//		log.info("@@ dataInfo = {}", dataInfo);
-//
-//		Map<String, Object> result = new HashMap<>();
-//		int statusCode = 0;
-//		String errorCode = null;
-//		String message = null;
-//		try {
-//			dataInfo =  dataService.getData(dataInfo);
-//			result.put("dataInfo", dataInfo);
-//		} catch(Exception e) {
-//			e.printStackTrace();
-//            statusCode = HttpStatus.INTERNAL_SERVER_ERROR.value();
-//            errorCode = "db.exception";
-//            message = e.getCause() != null ? e.getCause().getMessage() : e.getMessage();
-//		}
-//
-//		result.put("statusCode", statusCode);
-//		result.put("errorCode", errorCode);
-//		result.put("message", message);
-//		return result;
-//	}
-//
-//	/**
-//	 * Data Attribute 정보
-//	 * @param request
-//	 * @param dataAttribute
-//	 * @return
-//	 */
-//	@GetMapping(value = "detail-data-attribute")
-//	@ResponseBody
-//	public Map<String, Object> detailDataAttribute(HttpServletRequest request, DataAttribute dataAttribute) {
-//		log.info("@@ dataAttribute = {}", dataAttribute);
-//
-//		Map<String, Object> result = new HashMap<>();
-//		int statusCode = 0;
-//		String errorCode = null;
-//		String message = null;
-//		try {
-//			dataAttribute = dataService.getDataAttribute(dataAttribute.getDataId());
-//			result.put("dataAttribute", dataAttribute);
-//		} catch(Exception e) {
-//			e.printStackTrace();
-//            statusCode = HttpStatus.INTERNAL_SERVER_ERROR.value();
-//            errorCode = "db.exception";
-//            message = e.getCause() != null ? e.getCause().getMessage() : e.getMessage();
-//		}
-//
-//		result.put("statusCode", statusCode);
-//		result.put("errorCode", errorCode);
-//		result.put("message", message);
-//		return result;
-//	}
-//
-//	/**
-//	 * Data Origin Attribute 한건 등록
-//	 * @param request
-//	 * @param dataAttribute
-//	 * @return
-//	 */
-//	@PostMapping(value = "insert-data-attribute-file")
-//	@ResponseBody
-//	public Map<String, Object> insertDataAttributeFile(MultipartHttpServletRequest request) {
-//		log.info("@@ insert-data-attribute-file start");
-//
-//		Map<String, Object> result = new HashMap<>();
-//		int statusCode = 0;
-//		String errorCode = null;
-//		String message = null;
-//		try {
-//			Long dataId = Long.valueOf(request.getParameter("attributeFileDataId"));
-//			MultipartFile multipartFile = request.getFile("attributeFileName");
-//			// TODO
-//			FileInfo fileInfo = FileUtils.upload(multipartFile, FileUtils.DATA_ATTRIBUTE_UPLOAD, propertiesConfig.getDataAttributeUploadDir());
-//			if(fileInfo.getErrorCode() != null && !"".equals(fileInfo.getErrorCode())) {
-//				log.info("@@@@@@@@@@@@@@@@@@@@ error_code = {}", fileInfo.getErrorCode());
-//				result.put("statusCode", HttpStatus.BAD_REQUEST.value());
-//				result.put("errorCode", fileInfo.getErrorCode());
-//				result.put("message", message);
-//				return result;
-//			}
-//
-////			UserSession userSession = (UserSession)request.getSession().getAttribute(Key.USER_SESSION.name());
-////			fileInfo.setUserId(userSession.getUserId());
-////
-////			DataAttributeFileInfo dataFileInfo = getDataFileInfoFromFileInfo(fileInfo);
-////			dataFileInfo = dataFileInfoService.insertDataAttributeFile(dataId, dataFileInfo);
-////
-////			result.put("total_count", dataFileInfo.getTotalCount());
-////			result.put("parse_success_count", dataFileInfo.getParseSuccessCount());
-////			result.put("parse_error_count", dataFileInfo.getParseErrorCount());
-////			result.put("insert_success_count", dataFileInfo.getInsertSuccessCount());
-////			result.put("insert_error_count", dataFileInfo.getInsertErrorCount());
-////
-////			// 파일 삭제
-////			File copyFile = new File(dataFileInfo.getFilePath() + dataFileInfo.getFileRealName());
-////			if(copyFile.exists()) {
-////				copyFile.delete();
-////			}
-//		} catch(Exception e) {
-//			e.printStackTrace();
-//            statusCode = HttpStatus.INTERNAL_SERVER_ERROR.value();
-//            errorCode = "db.exception";
-//            message = e.getCause() != null ? e.getCause().getMessage() : e.getMessage();
-//		}
-//
-//		result.put("statusCode", statusCode);
-//		result.put("errorCode", errorCode);
-//		result.put("message", message);
-//		return result;
-//	}
-//
-//	/**
-//    * Map 에 데이터 표시
-//    * @param model
-//    * @return
-//	 * @throws JsonProcessingException
-//    */
-//    @GetMapping(value = "map-data")
-//    public String mapData(HttpServletRequest request, DataInfo dataInfo, Model model) throws JsonProcessingException {
-//
-//    	log.info("@@ map-data. dataInfo = {}", dataInfo);
-//
-//    	dataInfo = dataService.getData(dataInfo);
-//
-//    	GeoPolicy geoPolicy = geoPolicyService.getGeoPolicy();
-//    	String geoPolicyJson = "";
-//        try {
-//        	geoPolicyJson = objectMapper.writeValueAsString(geoPolicy);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//
-//        model.addAttribute("geoPolicyJson", geoPolicyJson);
-//        model.addAttribute("dataInfo", dataInfo);
-//
-//        return "/data/map-data";
-//    }
-//
-//    /**
-//     * TODO 고쳐야 함... .급해서
-//     * @param fileInfo
-//     * @return
-//     */
-//    private DataAttributeFileInfo getDataFileInfoFromFileInfo(FileInfo fileInfo) {
-//
-//    	DataAttributeFileInfo dataFileInfo = new DataAttributeFileInfo();
-//
-//    	dataFileInfo.setErrorCode(fileInfo.getErrorCode());
-//    	dataFileInfo.setErrorMessage(fileInfo.getErrorMessage());
-//    	dataFileInfo.setJobType(fileInfo.getJobType());
-//    	dataFileInfo.setUserId(fileInfo.getUserId());
-//    	dataFileInfo.setFileName(fileInfo.getFileName());
-//    	dataFileInfo.setFileRealName(fileInfo.getFileRealName());
-//    	dataFileInfo.setFilePath(fileInfo.getFilePath());
-//    	dataFileInfo.setFileSize(fileInfo.getFileSize());
-//    	dataFileInfo.setFileExt(fileInfo.getFileExt());
-//
-//    	return dataFileInfo;
-//    }
-
 	/**
 	 * 검색 조건
 	 * @param search
@@ -409,5 +245,14 @@ public class DataController {
 		StringBuffer buffer = new StringBuffer(dataInfo.getParameters());
 
 		return buffer.toString();
+	}
+	
+	private String roleValidator(HttpServletRequest request, Integer userGroupId, String roleName) {
+		List<String> userGroupRoleKeyList = CacheManager.getUserGroupRoleKeyList(userGroupId);
+        if(!RoleSupport.isUserGroupRoleValid(userGroupRoleKeyList, roleName)) {
+			request.setAttribute("httpStatusCode", 403);
+			return "/error/error";
+		}
+		return null;
 	}
 }
