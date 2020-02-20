97abc727f08a27fc57539d46e02b1cc2be8b33fb
hansangkim <gkstkd000@gmail.com>
2020-02-20 09:51:38 +0900

    - 컨트롤러, 샘플

---
+++ ndtp-user/src/main/java/ndtp/controller/GuideController.java
@@ -0,0 +1,43 @@
+package ndtp.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import ndtp.domain.GeoPolicy;
+import ndtp.service.GeoPolicyService;
+
+/**
+ * @author hansangkim
+ *
+ */
+@Controller
+@RequestMapping("/guide")
+public class GuideController {
+
+	@Autowired
+	private GeoPolicyService geoPolicyService;
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	/**
+	 * @param request
+	 * @param model
+	 * @return
+	 */
+	@GetMapping(value = "/help")
+	public String gotoApiHelp(HttpServletRequest request, Model model) {
+
+		GeoPolicy geoPolicy = geoPolicyService.getGeoPolicy();
+		try {
+			model.addAttribute("geoPolicyJson", objectMapper.writeValueAsString(geoPolicy));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return "/guide/help";
+	}
+}
