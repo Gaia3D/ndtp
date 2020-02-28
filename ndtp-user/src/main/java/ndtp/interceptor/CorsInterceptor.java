205392d30f6eeb0fbdfadcf48be0b0ba6c5a1d83
yhjeong1009 <yhjeong@gaia3d.com>
2020-02-28 11:14:25 +0900

    CORS 보안관련 기능 보완

--- ndtp-user/src/main/java/ndtp/interceptor/CorsInterceptor.java
+++ ndtp-user/src/main/java/ndtp/interceptor/CorsInterceptor.java
@@ -1,5 +1,8 @@
 package ndtp.interceptor;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -24,10 +27,16 @@ public class CorsInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String origin = request.getHeader(REQUEST_HEADER_ORIGIN);
         
+        // Bug Fix : The program defines an overly permissive Cross-Origin Resource Sharing (CORS) policy
+        if ("*".equals(origin)) return false;
+        
         // CORS 가능하도록 응답 헤더 추가
         if (StringUtils.hasLength(origin)) {
             // 요청한 도메인에 대해 CORS 를 허용한다. 제한이 필요하다면 필요한 값으로 설정한다.
-            response.setHeader(ACCESS_CONTROL_ALLOW_ORIGIN, origin);
+        	
+        	// Bug Fix : HTTP parameter directly written to HTTP header output in
+        	origin = URLEncoder.encode(origin, StandardCharsets.UTF_8.displayName());
+        	response.setHeader(ACCESS_CONTROL_ALLOW_ORIGIN, origin);
             
             // credentials 허용
             response.setHeader(ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
