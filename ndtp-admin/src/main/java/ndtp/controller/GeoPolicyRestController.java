5eddf2b427770727f94991d276fa9b857a20dd65
kimhjj <kimhj@gaia3d.com>
2020-02-28 18:40:34 +0900

    policy, geopolicy 요청 url변경, RestController 분리 #56

---
+++ ndtp-admin/src/main/java/ndtp/controller/GeoPolicyRestController.java
@@ -0,0 +1,102 @@
+package ndtp.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+import ndtp.config.CacheConfig;
+import ndtp.domain.CacheName;
+import ndtp.domain.CacheParams;
+import ndtp.domain.CacheType;
+import ndtp.domain.GeoPolicy;
+import ndtp.service.GeoPolicyService;
+
+@Slf4j
+@RestController
+@RequestMapping("/geopolicies")
+public class GeoPolicyRestController {
+
+	@Autowired
+	private CacheConfig cacheConfig;
+	@Autowired
+	private GeoPolicyService geoPolicyService;
+
+	@PostMapping(value = "/{geoPolicyId}")
+    public Map<String, Object> updateGeoPolicy(@Valid GeoPolicy geoPolicy, @PathVariable Long geoPolicyId, BindingResult bindingResult) {
+		Map<String, Object> result = new HashMap<>();
+		int statusCode = 0;
+		String errorCode = null;
+		String message = null;
+		try {
+			if(bindingResult.hasErrors()) {
+				message = bindingResult.getAllErrors().get(0).getDefaultMessage();
+				log.info("@@@@@ message = {}", message);
+				result.put("statusCode", HttpStatus.BAD_REQUEST.value());
+				result.put("errorCode", errorCode);
+				result.put("message", message);
+	            return result;
+			}
+			geoPolicy.setGeoPolicyId(geoPolicyId);
+			geoPolicyService.updateGeoPolicy(geoPolicy);
+
+			reloadCache();
+		} catch (Exception e) {
+			e.printStackTrace();
+            statusCode = HttpStatus.INTERNAL_SERVER_ERROR.value();
+            errorCode = "db.exception";
+            message = e.getCause() != null ? e.getCause().getMessage() : e.getMessage();
+		}
+		result.put("statusCode", statusCode);
+		result.put("errorCode", errorCode);
+		result.put("message", message);
+		return result;
+    }
+
+	@PostMapping(value = "/geoserver/{geoPolicyId}")
+    public Map<String, Object> updateGeoServer(@Valid GeoPolicy geoPolicy, @PathVariable Long geoPolicyId, BindingResult bindingResult) {
+		Map<String, Object> result = new HashMap<>();
+		int statusCode = 0;
+		String errorCode = null;
+		String message = null;
+		try {
+			if(bindingResult.hasErrors()) {
+				message = bindingResult.getAllErrors().get(0).getDefaultMessage();
+				log.info("@@@@@ message = {}", message);
+				result.put("statusCode", HttpStatus.BAD_REQUEST.value());
+				result.put("errorCode", errorCode);
+				result.put("message", message);
+	            return result;
+			}
+			geoPolicy.setGeoPolicyId(geoPolicyId);
+			geoPolicyService.updateGeoPolicyGeoServer(geoPolicy);
+
+			reloadCache();
+		} catch (Exception e) {
+			e.printStackTrace();
+            statusCode = HttpStatus.INTERNAL_SERVER_ERROR.value();
+            errorCode = "db.exception";
+            message = e.getCause() != null ? e.getCause().getMessage() : e.getMessage();
+		}
+		result.put("statusCode", statusCode);
+		result.put("errorCode", errorCode);
+		result.put("message", message);
+		return result;
+    }
+
+	private void reloadCache() {
+		CacheParams cacheParams = new CacheParams();
+		cacheParams.setCacheName(CacheName.GEO_POLICY);
+		cacheParams.setCacheType(CacheType.BROADCAST);
+		cacheConfig.loadCache(cacheParams);
+	}
+}
