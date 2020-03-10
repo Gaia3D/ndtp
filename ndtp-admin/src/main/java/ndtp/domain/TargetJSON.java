acac602bc145fe32e92d1d9103cc9a83fd16d167
yhjeong1009 <yhjeong@gaia3d.com>
2020-03-10 22:12:24 +0900

    json 변경 테스트 코드 작성

---
+++ ndtp-admin/src/main/java/ndtp/domain/TargetJSON.java
@@ -0,0 +1,12 @@
+package ndtp.domain;
+
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TargetJSON {
+	private List<Children> children; 
+}
---
+++ ndtp-user/src/main/java/ndtp/domain/SourceJSON.java
@@ -0,0 +1,17 @@
+package ndtp.domain;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class SourceJSON {
+	private List<DataInfo> datas;
+}
