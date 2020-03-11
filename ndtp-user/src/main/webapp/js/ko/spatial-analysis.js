f3e1accc96f126a28fd7c832fe9edb7971db2eec
kwunjaehyun <shinekill3@gmail.com>
2020-03-11 18:19:38 +0900

    - 공간분석 위험돔 분석 버그 픽스

--- ndtp-user/src/main/webapp/js/ko/spatial-analysis.js
+++ ndtp-user/src/main/webapp/js/ko/spatial-analysis.js
@@ -257,7 +257,7 @@ var SpatialAnalysis = function(magoInstance) {
 			if(!drawType) return false;
 			
 			var geographicCoord = result.clickCoordinate.geographicCoordinate;
-			if(!checkValidArea(geographicCoord, dataType, analysisType)) {
+			if(analysisType !== 'analysisRangeDome' && !checkValidArea(geographicCoord, dataType, analysisType)) {
 				alert('유효범위를 벗어났습니다.');
 				return false;
 			}
