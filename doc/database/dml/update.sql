b0708ee59c70f0b93a524e0427ad4339e14ce7ec
kwunjaehyun <shinekill3@gmail.com>
2020-03-24 19:51:55 +0900

    - UPDATE.SQL 속성 유무 데이터 인포 수정

--- doc/database/dml/update.sql
+++ doc/database/dml/update.sql
@@ -53,7 +53,21 @@ update data_info set data_type ='citygml' where data_group_id = 27;--busan-servi
 update data_info set data_type ='citygml' where data_group_id = 28;--busan-etc
 
 3 데이터 속성 존재 유무 수정
-update data_info set attribute_exist = true where data_group_id = xxxx
+update data_info set attribute_exist = true where data_group_id = 5;--sejong-apartment
+update data_info set attribute_exist = true where data_group_id = 6;--sejong-bus-sign
+update data_info set attribute_exist = true where data_group_id = 8;--sejong-curture
+update data_info set attribute_exist = true where data_group_id = 9;--sejong-etc
+update data_info set attribute_exist = true where data_group_id = 10;--sejong-industry
+update data_info set attribute_exist = true where data_group_id = 11;--sejong-jeonju
+update data_info set attribute_exist = true where data_group_id = 12;--sejong-pedestrian-light
+update data_info set attribute_exist = true where data_group_id = 13;--sejong-public
+update data_info set attribute_exist = true where data_group_id = 14;--sejong-road-sign
+update data_info set attribute_exist = true where data_group_id = 15;--sejong-safe-sign
+update data_info set attribute_exist = true where data_group_id = 16;--sejong-service
+update data_info set attribute_exist = true where data_group_id = 17;--sejong-street-lamp
+update data_info set attribute_exist = true where data_group_id = 18;--sejong-taxi-sign
+update data_info set attribute_exist = true where data_group_id = 19;--sejong-traffic-light
+
 
 4 그룹 기본 좌표 확인
 update data_group set location = ST_GeomFromText('POINT(경도 위도)', 4326) where data_group_id = xxxx;
