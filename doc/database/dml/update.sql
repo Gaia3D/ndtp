7d39194ad66f0d5567d139ac894b34f9cbf0aaf9
kwunjaehyun <shinekill3@gmail.com>
2020-03-24 16:07:50 +0900

    - 사용자 지도 페이지 카메라 높이 제한 해제 - update.sql 데이터 타입 업데이트 쿼리 갱신

--- doc/database/dml/update.sql
+++ doc/database/dml/update.sql
@@ -1,4 +1,4 @@
-update geopolicy set 
+seleupdate geopolicy set 
 	geoserver_data_url = 'http://localhost:8080/geoserver',
 	geoserver_data_workspace = 'ndtp',
 	geoserver_data_store ='ndtp',
@@ -6,7 +6,7 @@ update geopolicy set
 	geoserver_password = 'geoserver';
 
 
--- smart tiling 이후에 해 줘야 할 작업
+-- smart tiling 이후에 해 줘야 할 작업, 벌크 업로드 시 처리됨.
 1 데이터 건수 수정	
 update data_group set data_count = xxxx where data_group_key = '';
 update data_group set data_count = xxxx where data_group_key = '';
@@ -21,7 +21,36 @@ update data_group set data_count = xxxx where data_group_key = '';
 update data_group set data_count = xxxx where data_group_key = '';
 	
 2 데이터 타입 수정
-update data_info set data_type = 'xxxx' where data_group_id = xxxxx;
+--한줄따리
+update data_info set data_type ='citygml' where data_group_id < 10000;
+
+update data_info set data_type ='citygml' where data_group_id = 2;--busan-edc
+update data_info set data_type ='citygml' where data_group_id = 3;--busan-mj
+update data_info set data_type ='citygml' where data_group_id = 4;--busan-sv
+update data_info set data_type ='citygml' where data_group_id = 5;--sejong-apartment
+update data_info set data_type ='citygml' where data_group_id = 6;--sejong-bus-sign
+update data_info set data_type ='citygml' where data_group_id = 7;--sejong-complex
+update data_info set data_type ='citygml' where data_group_id = 8;--sejong-curture
+update data_info set data_type ='citygml' where data_group_id = 9;--sejong-etc
+update data_info set data_type ='citygml' where data_group_id = 10;--sejong-industry
+update data_info set data_type ='citygml' where data_group_id = 11;--sejong-jeonju
+update data_info set data_type ='citygml' where data_group_id = 12;--sejong-pedestrian-light
+update data_info set data_type ='citygml' where data_group_id = 13;--sejong-public
+update data_info set data_type ='citygml' where data_group_id = 14;--sejong-road-sign
+update data_info set data_type ='citygml' where data_group_id = 15;--sejong-safe-sign
+update data_info set data_type ='citygml' where data_group_id = 16;--sejong-service
+update data_info set data_type ='citygml' where data_group_id = 17;--sejong-street-lamp
+update data_info set data_type ='citygml' where data_group_id = 18;--sejong-taxi-sign
+update data_info set data_type ='citygml' where data_group_id = 19;--sejong-traffic-light
+update data_info set data_type ='citygml' where data_group_id = 20;--sejong-tree
+update data_info set data_type ='citygml' where data_group_id = 21;--busan-general-house
+update data_info set data_type ='citygml' where data_group_id = 22;--busan-apartment
+update data_info set data_type ='citygml' where data_group_id = 23;--busan-public
+update data_info set data_type ='citygml' where data_group_id = 24;--busan-industry
+update data_info set data_type ='citygml' where data_group_id = 25;--busan-curture
+update data_info set data_type ='citygml' where data_group_id = 26;--busan-welfare
+update data_info set data_type ='citygml' where data_group_id = 27;--busan-service
+update data_info set data_type ='citygml' where data_group_id = 28;--busan-etc
 
 3 데이터 속성 존재 유무 수정
 update data_info set attribute_exist = true where data_group_id = xxxx
