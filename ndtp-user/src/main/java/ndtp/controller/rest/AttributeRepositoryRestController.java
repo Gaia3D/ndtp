5e300da6030b42eb93f50d14cef2a0b10ec50e7a
jdcheon <jdcheon@gaia3d.com>
2020-03-28 15:30:06 +0900

    부산 명지 아파트 속성 처리 로직 수정

--- ndtp-user/src/main/java/ndtp/controller/rest/AttributeRepositoryRestController.java
+++ ndtp-user/src/main/java/ndtp/controller/rest/AttributeRepositoryRestController.java
@@ -34,9 +34,9 @@ public class AttributeRepositoryRestController {
 		int statusCode = HttpStatus.OK.value();
 		
 		// 01_mj_b_00218
-		String[] divideDataKey = dataKey.split("_");
-		String buildName = divideDataKey[0] + "_" + divideDataKey[3].substring(2);
-		result.put("attributeRepository", attributeRepositoryService.getDataAttribute(buildName));
+		//String[] divideDataKey = dataKey.split("_");
+		//String buildName = divideDataKey[0] + "_" + divideDataKey[3].substring(2);
+		result.put("attributeRepository", attributeRepositoryService.getDataAttribute(dataKey));
 		result.put("statusCode", statusCode);
 		result.put("errorCode", errorCode);
 		result.put("message", message);
